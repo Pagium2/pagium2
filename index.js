e8ba9c787542b6668191534b8fadc177fbd74a54
wurh <ro87630872@126.com>
2018-05-15 20:45:43 +0800

    【UPD】更新pagium2代码
    更新pagium2代码

--- index.js
+++ index.js
@@ -3,18 +3,19 @@
 var program = require("commander");
 var Alphabet = require('alphabetjs');
 var colors = require("colors");
+var cliCommand = require("pagium2-command-cli");
 
 var versionStr = require('./version.js');
 
 module.exports = {
     run: function (argv) {
         program
-            .version(Alphabet('PAGIUM2', 'stereo') + '\n v0.0.1')
+            .version(Alphabet('PAGIUM2', 'stereo') + '\n v1.0.1')
             .option('-i, --init', 'init pagium development environment ')
             .parse(process.argv);
 
         if (program.init) {
-            console.log('now we are init!');
+            cliCommand.run();
         } else {
             console.log("no command!");
             process.exit(1);
